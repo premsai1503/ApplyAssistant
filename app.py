4dc1c13cf45e0c00cc7b424aee308294d8eeda08
Arunakiran Mahadevamangalam <109696058+SumoKing69@users.noreply.github.com>
2025-02-28 04:05:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify, render_template, session, url_for , redirect
+from flask_session import Session
+import backend as bk
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route('/' , methods=['GET' , 'POST'])
+def index():            
+    return render_template('landing_page.html')
+
+@app.route('/form' , methods=['GET' , 'POST'])
+def form_page():
+    return render_template('form_page.html')
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    data = request.json
+    user_message = data.get("message")
+    bot_response = {"response": "The Chat facility is under development! So, you can upload the document based on the form displayed"} # Replace with actual chatbot logic
+    return jsonify(bot_response)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return jsonify({"response": "No file uploaded"}), 400
+    
+    file = request.files['file']
+    filename = file.filename
+    if filename == '':
+        return jsonify({"response": "No selected file"}), 400
+    
+    botresponse = bk.init(file)
+    return jsonify(botresponse)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend.py
@@ -0,0 +1,139 @@
+import base64
+import json
+import re
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, tool
+from together import Together
+from langchain.chat_models import init_chat_model
+import getpass
+import os
+from dotenv import load_dotenv
+from IPython.display import Markdown
+
+load_dotenv()
+
+if not os.environ.get("TOGETHER_API_KEY"):
+  os.environ["TOGETHER_API_KEY"] = os.getenv("TOGETHER_API_KEY")
+
+def encode_image(image_path):
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+
+def encode_image_from_file(image_file):
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+def load_image():
+    image_path = r"C:\Users\96895\Desktop\ApplyAI\sample_usa_passport.jpg"
+    img = encode_image(image_path)
+    return img
+
+def prompt_message(details,):
+    img = load_image()    
+    sys_msg = SystemMessage(
+        content=[
+            {
+                "type" : "text",
+                "text" : f"""You are an amazing and helpful AI assistant who helps in OCR(Optical Character Recognition) tasks.
+                            Your task is to identify these details: {details} in the given image. """
+            }
+        ]
+    )
+
+    human_msg = HumanMessage(
+        content=[
+            {
+                "type" : "text", 
+                # "text": f"""Find these details {details} from the image and return an JSON object with these {details} as keys.\n
+                #             Do not include any additional text or explanation. Output must be valid JSON only."""
+                # "text": f"Describe the given image and accurately extract any text present in it. Then, find the specified details: {details}."  
+                #         "If any details are not found, return them as `NOT FOUND`."
+                "text" : "Provide the results as JSON object only."
+            },
+            {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{img}"},
+            },
+        ]
+    )
+
+    ai_msg = AIMessage(
+        content=[
+            {
+                "type" : "text",
+                "text" : """{
+                                "FirstName": "Nicholas",
+                                "LastName": "Burwell",
+                                "Sex": "Male",
+                                "PassportNumber": "426012744",
+                                "PermanentAccountNumber": "NOT FOUND",
+                                "Nationality": "United States of America",
+                                "DateofBirth": "18 Oct 1978",
+                                "PlaceofBirth": "California, USA",
+                                "DateofIssue": "14 Dec 2017",
+                                "DateofExpiration": "13 Dec 2027"
+                        }"""
+            }
+        ]
+    )
+    messages = [sys_msg, human_msg, ai_msg]
+    return messages
+     
+
+def zero_shot_learning(details):
+    messages = prompt_message(details)
+    model = init_chat_model("meta-llama/Llama-Vision-Free", model_provider="together")
+    return model , messages
+    
+def get_response(model, messages, document , details):
+    try:
+        document = encode_image_from_file(document)
+        prompt = HumanMessage(
+            content=[
+                {
+                    "type" : "text", 
+                    # "text": f"""Find these details {details} from the image and return an JSON object with these {details} as keys.\n
+                    #             Do not include any additional text or explanation. Output must be valid JSON only."""
+                    # "text": f"Describe the given image and accurately extract any text present in it. Then, find the specified details: {details}."  
+                    #         "If any details are not found, return them as `NOT FOUND`."
+                    "text" : f"Thats good. Now do the same on this image. Provide the results in a JSON object like {details}. Do not include explanations, extra text, or formatting characters."
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/jpeg;base64,{document}"},
+                },
+            ]
+        )
+
+        messages.append(prompt)
+        response = model.invoke(messages)
+        return response.content
+    except Exception as e:
+        return {"response": f"Error processing file: {str(e)}"}
+
+def jsonification(response):
+    """Extracts and fixes the JSON content from a model response."""
+    try:
+    # Extract JSON from response (removes extra text)
+        match = re.search(r'\{.*\}', response, re.DOTALL)
+        if match:
+            # Extract JSON part
+            json_str = match.group(0)  
+            # Remove backslashes before `{}` if present
+            json_str = json_str.replace("\\{", "{").replace("\\}", "}")
+            # Fix formatting issues
+            json_str = json_str.strip().replace("\n", "").replace("\'", "\"")
+            # Convert to dictionary
+            return json.loads(json_str)
+        else:
+            return {"error": "Could not extract JSON from response."}
+    except json.JSONDecodeError:
+        return {"error": "Invalid JSON format."}
+
+def init(document):
+    labels = [
+    "First Name", "Last Name", "Sex","Passport Number","Permanent Account Number", "Nationality",
+    "Date of Birth", "Place of Birth", "Date of Issue", "Date of Expiration"
+    ]
+    details = ", ".join(labels)
+    model , messages = zero_shot_learning(details)
+    response = get_response(model, messages, document , details)
+    return json.loads(response)
